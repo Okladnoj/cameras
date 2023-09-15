dc884b05f7f60d808144d710ce35c7f73b830c82
Okladnoj <dmytrijokladnoj@gmail.com>
2023-09-15 17:33:52 +0300

    Added steppable request permissions

--- lib/src/web/cameras_web.dart
+++ lib/src/web/cameras_web.dart
@@ -7,7 +7,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import '../models/camera_description/camera_description.dart';
 import '../models/enums/supported_platforms.dart';
 import '../platforms/cameras_platform_interface.dart';
-import '../utils/logger/logger.dart';
 
 class CamerasWeb extends CamerasPlatform {
   CamerasWeb();
@@ -31,86 +30,69 @@ class CamerasWeb extends CamerasPlatform {
 
   @override
   Future<List<CameraDescription>> getAvailableCameras() async {
+    List<CameraDescription> availableCameras = [];
     try {
-      // Check if mediaDevices is not null. If it's null, then the browser might not support this feature.
       await _navigatorRequestPermissions();
-      await _mediaDevicesRequestPermissions();
+      await Future.delayed(const Duration(milliseconds: 150));
+      await _requestPermissionsForAllCameras();
 
       final mediaDevices = _navigator.mediaDevices;
-
       if (mediaDevices == null) {
         log('MediaDevices is null. The browser might not support this feature.');
         return [];
       }
 
-      // Enumerate all media devices. This will give us a list of all audio and video input devices.
       final devices = await mediaDevices.enumerateDevices();
-
-      logger.w('devices => $devices');
-
-      // Filter out only the video input devices from the list.
-      final videoDevices = devices.where((device) {
-        return device.kind == 'videoinput';
-      }).toList();
-
-      logger.w('videoDevices => $videoDevices');
+      final videoDevices =
+          devices.where((device) => device.kind == 'videoinput').toList();
 
       // Map the list of video input devices to a list of CameraDescription objects.
-      return videoDevices
+      availableCameras = videoDevices
           .map((device) => CameraDescription.fromJs(videoDevices, device))
           .toList();
     } catch (error) {
       log('An error occurred while getting available cameras: $error');
-      return [];
     }
+    return availableCameras;
   }
 
   Future<void> _navigatorRequestPermissions() async {
     try {
-      // Request the browser for access to the camera by calling getUserMedia.
-      // This will trigger a permission request to the user.
-      // To ensure only video is used, you can set audio: false.
+      // Request access to the camera, making the constraints less strict.
       final cameraStream = await _navigator.getUserMedia(
         video: {
-          'mandatory': {'minWidth': 1280, 'minHeight': 720, 'minFrameRate': 30},
-          'optional': []
+          'facingMode': 'user',
+          'width': {'ideal': 1280},
+          'height': {'ideal': 720}
         },
         audio: false,
       );
 
-      logger.w('cameraStream.id => ${cameraStream.id}');
-
-      // Since we've specified audio: false, this step may not be necessary
-      // But in case you decide to enable audio later, it's good to keep.
+      // Stop any audio tracks if they have been started
       cameraStream.getAudioTracks().forEach((track) => track.stop());
-
-      logger.w('cameraStream => $cameraStream');
     } catch (e) {
-      // Handle the error
-      logger.e('Error accessing camera: $e');
+      log('Error accessing camera: $e');
     }
   }
 
-  Future<void> _mediaDevicesRequestPermissions() async {
+  Future<void> _requestPermissionsForAllCameras() async {
     try {
-      // Request the browser for access to the camera by calling getUserMedia.
-      // This will trigger a permission request to the user.
-      // To ensure only video is used, you can set audio: false.
+      final mediaDevices = _navigator.mediaDevices;
+      if (mediaDevices != null) {
+        final devices = await mediaDevices.enumerateDevices();
+        final videoDevices = devices.where((device) {
+          return device.kind == 'videoinput';
+        }).toList();
 
-      final cameraStream = await _navigator.mediaDevices?.getUserMedia({
-        'video': {
-          'mandatory': {'minWidth': 1280, 'minHeight': 720, 'minFrameRate': 30},
-          'optional': []
-        },
-        'audio': false,
-      });
-
-      // Since we've specified audio: false, this step may not be necessary
-      // But in case you decide to enable audio later, it's good to keep.
-      cameraStream?.getAudioTracks().forEach((track) => track.stop());
+        for (var device in videoDevices) {
+          await _navigator.getUserMedia(
+            video: {'deviceId': device.deviceId},
+            audio: false,
+          );
+        }
+      }
     } catch (e) {
-      // Handle the error
-      logger.e('Error accessing camera: $e');
+      log('Error requesting permissions for all cameras: $e');
     }
   }
 }
